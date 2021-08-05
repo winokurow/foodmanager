d15ebc3df7101cd3949dfa4db6fe9694f66cc76c
Ilja Winokurow <ilja_winokurow@yahoo.com>
2021-08-05 18:14:52 +0200

    Add liquibase

---
+++ src/main/java/de/zottig/foodmanager/controllers/service/PlaceService.java
@@ -0,0 +1,28 @@
+package de.zottig.foodmanager.controllers.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import de.zottig.foodmanager.models.Place;
+import de.zottig.foodmanager.repository.PlaceRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class PlaceService {
+
+	private PlaceRepository placeRepository;
+
+	public List<Place> findAll() {
+		return (List<Place>) placeRepository.findAll();
+	}
+
+	public Place insert(Place food) {
+		return placeRepository.save(food);
+	}
+
+	public void deleteById(Long id) {
+		placeRepository.deleteById(id);
+	}
+}
